b9d55fa400769af6e2cc159c869343c3b563a8ed
chanuthdias <s.chanuthddias@gmail.com>
2025-03-31 11:09:19 +0530

    basic implementation to home_screen

--- lib/UI/screens/home/home_screen.dart
+++ lib/UI/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_api_app/core/const.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,41 +9,81 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Movie Ratings'),
-        centerTitle: true,
         backgroundColor: AppColors.primaryBlue,
+        elevation: 0,
+        title: const Text('FLICKER PLAY'),
+        centerTitle: true,
       ),
-      backgroundColor: AppColors.background,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Top Rated Movies',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: AppColors.textColor,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                // controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search for movies...',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                ),
+                onChanged: (value) {
+                  // Handle search logic here
+                },
               ),
-            ),
-            SizedBox(height: 20),
-            MovieCard(
-              title: 'Inception',
-              rating: 8.8,
-              imageUrl: 'https://via.placeholder.com/150',
-            ),
-            MovieCard(
-              title: 'The Dark Knight',
-              rating: 9.0,
-              imageUrl: 'https://via.placeholder.com/150',
-            ),
-            MovieCard(
-              title: 'Interstellar',
-              rating: 8.6,
-              imageUrl: 'https://via.placeholder.com/150',
-            ),
-          ],
+
+              const SizedBox(height: 32),
+
+              Text('Trending Movies', style: GoogleFonts.aBeeZee(fontSize: 25)),
+
+              const SizedBox(height: 32),
+
+              SizedBox(
+                child: MovieCard(
+                  title: 'Inception',
+                  rating: 8.8,
+                  imageUrl: 'https://via.placeholder.com/150',
+                ),
+              ),
+
+              const SizedBox(height: 32),
+
+              Text(
+                'Top Rated Movies',
+                style: GoogleFonts.aBeeZee(fontSize: 25),
+              ),
+
+              const SizedBox(height: 32),
+
+              SizedBox(
+                child: MovieCard(
+                  title: 'Inception',
+                  rating: 8.8,
+                  imageUrl: 'https://via.placeholder.com/150',
+                ),
+              ),
+
+              const SizedBox(height: 32),
+
+              Text('Upcoming Movies', style: GoogleFonts.aBeeZee(fontSize: 25)),
+
+              const SizedBox(height: 32),
+
+              SizedBox(
+                child: MovieCard(
+                  title: 'Inception',
+                  rating: 8.8,
+                  imageUrl: 'https://via.placeholder.com/150',
+                ),
+              ),
+
+              const SizedBox(height: 32),
+            ],
+          ),
         ),
       ),
     );
